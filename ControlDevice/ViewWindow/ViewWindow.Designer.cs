b34548517aa8c16571165ee45e6a202a854fbde8
gish5505 <gish5505@users.noreply.github.com>
2019-04-08 00:54:31 +0300

    minor

--- ControlDevice/ViewWindow/ViewWindow.Designer.cs
+++ ControlDevice/ViewWindow/ViewWindow.Designer.cs
@@ -36,7 +36,6 @@
             this.voltageBox = new System.Windows.Forms.TextBox();
             this.outputPendingBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
             this.outputActiveBox = new System.Windows.Forms.TextBox();
@@ -44,12 +43,16 @@
             this.voltageAverageBox = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(610, 285);
+            this.btnStart.Location = new System.Drawing.Point(787, 300);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(136, 41);
             this.btnStart.TabIndex = 1;
@@ -59,16 +62,16 @@
             // 
             // voltageBox
             // 
-            this.voltageBox.Location = new System.Drawing.Point(240, 40);
+            this.voltageBox.Location = new System.Drawing.Point(9, 32);
             this.voltageBox.Name = "voltageBox";
             this.voltageBox.ReadOnly = true;
-            this.voltageBox.Size = new System.Drawing.Size(64, 20);
+            this.voltageBox.Size = new System.Drawing.Size(195, 20);
             this.voltageBox.TabIndex = 6;
             this.voltageBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // outputPendingBox
             // 
-            this.outputPendingBox.Location = new System.Drawing.Point(12, 34);
+            this.outputPendingBox.Location = new System.Drawing.Point(9, 33);
             this.outputPendingBox.Name = "outputPendingBox";
             this.outputPendingBox.Size = new System.Drawing.Size(64, 20);
             this.outputPendingBox.TabIndex = 7;
@@ -78,21 +81,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Enabled = false;
-            this.label1.Location = new System.Drawing.Point(237, 24);
+            this.label1.Location = new System.Drawing.Point(6, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(142, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Вольтаж на магнетроне, В";
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 18);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(178, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Выставленное значение тока, мА";
             // 
             // chart1
             // 
@@ -100,7 +94,7 @@
             this.chart1.BorderlineColor = System.Drawing.Color.Gray;
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Location = new System.Drawing.Point(12, 168);
+            this.chart1.Location = new System.Drawing.Point(12, 28);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
             series1.ChartArea = "ChartArea1";
@@ -109,7 +103,7 @@
             series1.Name = "Series1";
             series1.YValuesPerPoint = 2;
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(605, 136);
+            this.chart1.Size = new System.Drawing.Size(685, 299);
             this.chart1.TabIndex = 10;
             this.chart1.Text = "chart1";
             title1.Alignment = System.Drawing.ContentAlignment.TopCenter;
@@ -132,9 +126,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Location = new System.Drawing.Point(118, 33);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 32);
+            this.button1.Size = new System.Drawing.Size(89, 26);
             this.button1.TabIndex = 11;
             this.button1.Text = "Применить";
             this.button1.UseVisualStyleBackColor = true;
@@ -142,35 +136,35 @@
             // 
             // outputActiveBox
             // 
-            this.outputActiveBox.Location = new System.Drawing.Point(240, 130);
+            this.outputActiveBox.Location = new System.Drawing.Point(6, 132);
             this.outputActiveBox.Name = "outputActiveBox";
             this.outputActiveBox.ReadOnly = true;
-            this.outputActiveBox.Size = new System.Drawing.Size(64, 20);
+            this.outputActiveBox.Size = new System.Drawing.Size(198, 20);
             this.outputActiveBox.TabIndex = 12;
             this.outputActiveBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(237, 113);
+            this.label3.Location = new System.Drawing.Point(3, 116);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(201, 13);
             this.label3.TabIndex = 13;
             this.label3.Text = "Активное значение тока на выход, мА";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // voltageBoxAverage
+            // voltageAverageBox
             // 
-            this.voltageAverageBox.Location = new System.Drawing.Point(240, 89);
-            this.voltageAverageBox.Name = "voltageBoxAverage";
+            this.voltageAverageBox.Location = new System.Drawing.Point(9, 82);
+            this.voltageAverageBox.Name = "voltageAverageBox";
             this.voltageAverageBox.ReadOnly = true;
-            this.voltageAverageBox.Size = new System.Drawing.Size(64, 20);
+            this.voltageAverageBox.Size = new System.Drawing.Size(195, 20);
             this.voltageAverageBox.TabIndex = 14;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(237, 69);
+            this.label4.Location = new System.Drawing.Point(6, 66);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(136, 13);
             this.label4.TabIndex = 15;
@@ -178,34 +172,57 @@
             // 
             // statusStrip1
             // 
-            this.statusStrip1.Location = new System.Drawing.Point(0, 329);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 362);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(758, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(946, 22);
             this.statusStrip1.TabIndex = 16;
             this.statusStrip1.Text = "statusStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.outputPendingBox);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(703, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 80);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выставленное значение тока, мА";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.voltageBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.voltageAverageBox);
+            this.groupBox2.Controls.Add(this.outputActiveBox);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(703, 126);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(220, 168);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
             // 
             // ViewWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ClientSize = new System.Drawing.Size(758, 351);
+            this.ClientSize = new System.Drawing.Size(946, 384);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.voltageAverageBox);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.outputActiveBox);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.outputPendingBox);
-            this.Controls.Add(this.voltageBox);
             this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
             this.Name = "ViewWindow";
             this.Text = "magcontrol";
             this.Load += new System.EventHandler(this.ViewWindow_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,7 +234,6 @@
         private System.Windows.Forms.TextBox voltageBox;
         private System.Windows.Forms.TextBox outputPendingBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox outputActiveBox;
@@ -225,6 +241,8 @@
         private System.Windows.Forms.TextBox voltageAverageBox;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
